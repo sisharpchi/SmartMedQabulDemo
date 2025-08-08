a9294b496543f4f7520981392ea50829963d2c5e
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2025-08-08 21:29:45 +0500

    changes

---
+++ src/Application/Abstractions/Repositories/IDoctorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Abstractions.Repositories;
+
+public interface IDoctorRepository
+{
+    Task<long> AddDoctorAsync(Doctor doctor);
+    Task<Doctor> GetDoctorByIdAsync(long id);
+    Task<ICollection<Doctor>> GetAllDoctorsAsync();
+    Task UpdateDoctorAsync(Doctor doctor);
+    Task DeleteDoctorAsync(long id);
+
+    Task<Doctor> GetAllDoctorsByHospitalIdAsync(long hospitalId);
+}
