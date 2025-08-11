38ce2615ebc4d1a53f1ea283bba841a0b8929bb1
mrbuzb <mrbpubg14@gmail.com>
2025-08-11 12:24:09 +0500

    UnConfirmedDoctorEndpoint

---
+++ src/Application/Dtos/UnConfirmedDoctorGetDto.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Application.Dtos;
+
+public class UnConfirmedDoctorGetDto
+{
+    public long Id { get; set; }
+    public bool IsConfirmedByAdmin { get; set; }
+    public UserGetDto User { get; set; }
+}
---
+++ src/Application/Services/DoctorService.cs
@@ -0,0 +1,40 @@
+﻿using Application.Abstractions.Repositories;
+using Application.Abstractions.Services;
+using Application.Dtos;
+using Domain.Entities;
+
+namespace Application.Services;
+
+public class DoctorService(IDoctorRepository _doctorRepo) : IDoctorService
+{
+    public async Task<ICollection<UnConfirmedDoctorGetDto>> GetAllUnConfirmedDoctors()
+    {
+        var doctors  = await _doctorRepo.GetAllUnConfirmedDoctorsAsync();
+        return doctors.Select(Converter).ToList();
+    }
+
+    private UnConfirmedDoctorGetDto Converter(Doctor doctor)
+    {
+        return new UnConfirmedDoctorGetDto
+        {
+            Id = doctor.Id,
+            IsConfirmedByAdmin = doctor.IsConfirmedByAdmin,
+            User = Converter(doctor.User)
+        };
+    }
+
+    private UserGetDto Converter(User user)
+    {
+        return new UserGetDto
+        {
+            Email = user.Confirmer!.Gmail,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            PhoneNumber = user.PhoneNumber,
+            UserId = user.UserId,
+            UserName = user.UserName,
+            Role = user.Role.Name,
+        };
+    }
+
+}
